c510a1f4701481c9ece7586f71d9153880599943
wda521 <272264169@qq.com>
2017-04-14 10:07:04 +0800

    测试提交

--- js/login.js
+++ js/login.js
@@ -5,11 +5,11 @@ document.getElementById("loginIn").addEventListener("tap", function () {
     var userId = document.getElementById("userId").value;
     var userPsw = document.getElementById("userPsw").value;
     if (userId.length == 0) {
-        $.alert("请输入您的用户名","温馨提示");
+        $("#errorTip").html("请输入您的用户名");
         return;
     }
     if (userPsw.length == 0) {
-        $.alert("请输入您的密码","温馨提示");
+        $("#errorTip").html("请输入您的密码");
         return
     }
     var param = {
