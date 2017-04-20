71735c1264165ef7baa088d2f182c3b43b722e99
wda521 <272264169@qq.com>
2017-04-20 08:07:49 +0800

    测试提交

--- js/login.js
+++ js/login.js
@@ -34,7 +34,7 @@ function getPosition() {
             // 指示浏览器获取高精度的位置，默认为false
             enableHighAcuracy: true,
             // 指定获取地理位置的超时时间，默认不限时，单位为毫秒
-            timeout: 5000,
+            //timeout: 5000,
             // 最长有效期，在重复获取地理位置时，此参数指定多久再次获取位置。
             maximumAge: 3000
         });
