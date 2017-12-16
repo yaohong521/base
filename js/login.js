0cd1456efe4b2a2c2ad9e897eda4eff38c200275
wda521 <272264169@qq.com>
2017-12-16 10:53:47 +0800

    测试

--- js/login.js
+++ js/login.js
@@ -5,19 +5,21 @@ document.getElementById("loginIn").addEventListener("tap", function () {
     var userId = document.getElementById("userId").value;
     var userPsw = document.getElementById("userPsw").value;
     if (userId.length == 0) {
-        $("#errorTip").html("请输入您的用户名");
+        $("#errorTip").html("用户名为空");
         return;
     } else {
         $("#errorTip").html("");
     }
     if (userPsw.length == 0) {
-        $("#errorTip").html("请输入您的密码");
+        $("#errorTip").html("密码为空");
         return
     } else {
         $("#errorTip").html("");
     }
     if (1 == 1) {
-        $.alert("success", "温馨提示")
+        $.alert("success", "温馨提示",function () {
+            window.location.href='views/index.html'
+        })
     }
     var param = {
         USER_ID: userId,
