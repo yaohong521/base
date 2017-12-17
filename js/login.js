0c025c2c85bc5246dd14482d20862f8a7c57bd41
wda521 <272264169@qq.com>
2017-12-17 11:38:06 +0800

    login页面选择

--- js/login.js
+++ js/login.js
@@ -17,17 +17,17 @@ document.getElementById("loginIn").addEventListener("tap", function () {
         $("#errorTip").html("");
     }
     if (1 == 1) {
-        $.alert("success", "温馨提示",function () {
-            window.location.href='views/index.html'
+        $.alert("进入下一版登录界面", "温馨提示", function () {
+            window.location.href = 'views/index.html'
         })
     }
-    var param = {
-        USER_ID: userId,
-        USER_PASSWORD: userPsw
-    };
-    Wda.Ajax.doPost(Config.ucenter + "/getUser", param).then(function (data) {
-
-    }, function (error) {
-
-    });
+    // var param = {
+    //     USER_ID: userId,
+    //     USER_PASSWORD: userPsw
+    // };
+    // Wda.Ajax.doPost(Config.ucenter + "/getUser", param).then(function (data) {
+    //
+    // }, function (error) {
+    //
+    // });
 });
